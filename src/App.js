ca0b1de725d2a141f6cdf1f0c1a0ce7ce5a60af5
Dhruvil Shah <dhruvill@amazon.com>
2024-02-07 01:17:56 -0500

    Initial Setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  
+  useEffect(() => {
+    axios.get("http://localhost:3002").then((res) => {
+      console.log(res.data);
+    })
+  }, [])
+
+  let loginSuccess = () => {
+    axios.post("http://localhost:3002/login", {username: "admin", password: 'admin'}).then((res) => {
+      console.log(res.data);
+    })
+  }
+
+  let loginFail = () => {
+    axios.post("http://localhost:3002/login", {username: "non-admin", password: 'non-admin'}).then((res) => {
+      console.log(res.data);
+    })
+  }
+
+
+  
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello
         </p>
         <a
           className="App-link"
@@ -17,6 +41,18 @@ function App() {
         >
           Learn React
         </a>
+        <button 
+          style={{marginTop: 20, backgroundColor: 'green', color: 'white'}}
+          onClick={loginSuccess}
+        >
+          Login Successful Call
+        </button>
+        <button 
+          style={{marginTop: 20, backgroundColor: 'red', color: 'white'}}
+          onClick={loginFail}
+        >
+          Login Fail Call
+        </button>
       </header>
     </div>
   );
